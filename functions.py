73a2458708648ba12308b6bb3714412e280cacdd
diegotrevisancc <diego.trevisan.cc@gmail.com>
2022-10-18 16:49:24 -0300

    fix some bugs

--- functions.py
+++ functions.py
@@ -2,36 +2,43 @@ import os
 def limpar_tela(): 
     os.system("cls")
 
-def exibe_jogo(palavra_chave, dica_1, dica_2, dica_3):
-    limpar_tela()
-    numero_caracteres = len(palavra_chave)
+def gera_palavra_censurada(palavra_chave): 
     palavra_censurada = ""
-    dicas = [dica_1, dica_2, dica_3]
-    contador = 0
+    numero_caracteres = len(palavra_chave)
     for index in range(0, numero_caracteres): 
         palavra_censurada += "*"
-    print("A palavra chave possuí {} letras." .format(numero_caracteres))
+    return palavra_censurada
+def exibe_jogo(palavra_chave, dica_1, dica_2, dica_3, palavra_censurada):
+    dicas = [dica_1, dica_2, dica_3]
+    contador = 0
+    print("A palavra chave possuí {} letras." .format(len(palavra_chave)))
     print(palavra_censurada)
     print("Receber Dica (1)")
     print("Adivinhar Letra(2)")
-    return palavra_censurada
+    print('---------------------\n')
         
 def mostra_dica(dicas, contador):
     print(dicas[contador])
     contador+=1
     return contador
 
-def compara_chute(chute, palavra_chave, palavra_censurada): 
+def compara_chute(chute, palavra_chave, palavra_censurada, erros): 
     chute = chute.upper()
     palavra_chave = palavra_chave.upper()
     indice = 0 
-    palavra_censurada_nova = []
-    
-    for letra in palavra_chave:
-        if chute == letra:
-            palavra_censurada_nova.append(letra)
-            continue
+    palavra_censurada_nova = list(palavra_censurada)
+    acertou = False
+    for index in range(0, len(palavra_chave)):
+        if palavra_chave[index] == chute:
+            palavra_censurada_nova[index] = chute
+            acertou = True
         else:
-            palavra_censurada_nova.append("*")
             continue
-    return(''.join(palavra_censurada_nova))
+
+    if acertou == False:
+        erros+=1
+    else:
+        pass
+
+    retorno = [(''.join(palavra_censurada_nova)), erros]
+    return retorno
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import limpar_tela, exibe_jogo, mostra_dica, compara_chute
+from functions import limpar_tela, exibe_jogo, mostra_dica, compara_chute, gera_palavra_censurada
 while True:
     competidor = input("Insira o nome do competidor: ")
     desafiante = input("Insira o nome do desafiante: ")
@@ -9,14 +9,43 @@ while True:
     dica_1 = input(desafiante + " insira a dica número 1: ")
     dica_2 = input(desafiante + " insira a dica número 2: ")
     dica_3 = input(desafiante + " insira a dica número 3: ")
+    rodada = True
+    limpar_tela()
     
-    palavra_censurada = exibe_jogo(palavra_chave, dica_1, dica_2, dica_3)
-    op = input("Insira uma opção: ")
+    palavra_censurada = gera_palavra_censurada(palavra_chave)
+
     dicas = [dica_1, dica_2, dica_3]
     contador_dicas = 0
-    if op == "1":
-        mostra_dica(dicas, contador_dicas)
-        chute = input("Chute uma letra: ")
-        palavra_censurada = compara_chute(chute, palavra_chave, palavra_censurada)
+    erros = 0
+    
+    while (rodada):
+        exibe_jogo(palavra_chave, dica_1, dica_2, dica_3, palavra_censurada)
+        op = input("Insira uma opção: ")        
+        if op == "1": #Só pode ser chamada 3 vezes, tem que ajeitar quando finalizar o code
+            contador_dicas = mostra_dica(dicas, contador_dicas)
+            chute = input("Chute uma letra: ")
+            comparacao = compara_chute(chute, palavra_chave, palavra_censurada, erros)
+            palavra_censurada = comparacao[0]
+            erros = comparacao[1]
+            print(palavra_censurada)
+            print("Erros: " + str(erros))
+            print('---------------------\n')
+        elif op == "2":
+            chute = input("Chute uma letra: ")
+            comparacao = compara_chute(chute, palavra_chave, palavra_censurada, erros)
+            palavra_censurada = comparacao[0]
+            erros = comparacao[1]
+            print(palavra_censurada)
+            print("Erros: " + str(erros))
+            print('---------------------\n')
+            
+        if erros == 5:
+            rodada = False
+            print("O Desafiante {} venceu!" .format(desafiante))
+            
         
+        if palavra_censurada == palavra_chave:
+            rodada = False
+            print("O Competidor {} venceu!" .format(competidor))
+
         
